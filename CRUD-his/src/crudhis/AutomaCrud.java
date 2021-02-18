d09bcaf94da47d21d555daeab20f6275ea0f4afb
Matteo Moretti <78532093+matteo2025@users.noreply.github.com>
2021-02-18 15:21:08 +0100

    aggiunta UI

--- CRUD-his/src/crudhis/AutomaCrud.java
+++ CRUD-his/src/crudhis/AutomaCrud.java
@@ -1,63 +1,93 @@
-
 package crudhis;
 
 public class AutomaCrud implements State {
-    
+
     private State stato;
 
     @Override
     public void next(Event e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    private class Ricerca implements State{
+
+    private class Ricerca implements State {
 
         @Override
         public void next(Event e) {
-            if (e instanceof RicercaEvent){
-            }else if(){
+            if (e instanceof RicercaEvent) {
+
+            } else if (e instanceof AddEvent) {
                 stato = new Aggiungi();
-             }
-                
+            } else if (e instanceof SelezionaEvent) {
+                stato = new Visualizza();
+            } else {
+                System.out.println("errore");
             }
-        
-         
-            
-        }
-        
-         private class Aggiungi implements State{
 
-        @Override
-        public void next(Event e) {
-         
-            
         }
-        
-         private class Visualizza implements State{
 
-        @Override
-        public void next(Event e) {
-         
-            
-        }
-        
-         private class Rimuovi implements State{
-
-        @Override
-        public void next(Event e) {
-         
-            
-        }
-        
-         private class Modifica implements State{
-
-        @Override
-        public void next(Event e) {
-         
-            
-        }
-        
-        
     }
-    
- }
+
+    private class Aggiungi implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof ConfermaEvent) {
+                stato = new Visualizza();
+            } else if (e instanceof AnnullaEvent) {
+                stato = new Ricerca();
+            } else {
+                System.out.println("errore");
+            }
+        }
+    }
+
+    private class Visualizza implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof AddEvent) {
+                stato = new Aggiungi();
+            } else if (e instanceof SelezionaEvent) {
+
+            } else if (e instanceof ModificaEvent) {
+                stato = new Modifica();
+            } else if (e instanceof RimuoviEvent) {
+                stato = new Rimuovi();
+            } else if (e instanceof RicercaEvent) {
+                stato = new Ricerca();
+            } else {
+                System.out.println("errore");
+            }
+        }
+    }
+
+    private class Rimuovi implements State {
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof ConfermaEvent) {
+                stato = new Ricerca();
+            } else if (e instanceof AnnullaEvent) {
+                stato = new Visualizza();
+            } else {
+                System.out.println("errore");
+            }
+        }
+    }
+
+    private class Modifica implements State {
+
+        @Override
+        public void next(Event e) {
+            if(e instanceof ConfermaEvent){
+                stato = new Visualizza();
+            }else if(e instanceof AnnullaEvent){
+                stato = new Visualizza();
+            }else{
+                System.out.println("errore");
+            }
+        }
+
+    }
+
+}
