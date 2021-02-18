e073212d64b7b7460c21b838fc19ce94932413a3
nicoloabatecola <78588208+nicoloabatecola@users.noreply.github.com>
2021-02-18 15:00:49 +0100

    UPDATE

--- CRUD-his/src/crudhis/AutomaCrud.java
+++ CRUD-his/src/crudhis/AutomaCrud.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package crudhis;
 
-/**
- *
- * @author Userù
- */
-public class AutomaCrud {
+public class AutomaCrud implements State {
     
-}
+    private State stato;
+
+    @Override
+    public void next(Event e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private class Ricerca implements State{
+
+        @Override
+        public void next(Event e) {
+            if (e instanceof RicercaEvent){
+            }else if(){
+                stato = new Aggiungi();
+             }
+                
+            }
+        
+         
+            
+        }
+        
+         private class Aggiungi implements State{
+
+        @Override
+        public void next(Event e) {
+         
+            
+        }
+        
+         private class Visualizza implements State{
+
+        @Override
+        public void next(Event e) {
+         
+            
+        }
+        
+         private class Rimuovi implements State{
+
+        @Override
+        public void next(Event e) {
+         
+            
+        }
+        
+         private class Modifica implements State{
+
+        @Override
+        public void next(Event e) {
+         
+            
+        }
+        
+        
+    }
+    
+ }
